b3c8e9ff2ec61cba4473d86d6ef58f4af88e4e21
Lukas Bamert <bamert@protonmail.com>
2023-10-19 10:38:20 +0200

    Man this formula from the paper does not work

--- testing/tests/test_monte_mul.c
+++ testing/tests/test_monte_mul.c
@@ -13,10 +13,17 @@ extern void monte_reduce(uint64_t *a, uint64_t *b);
 
 extern void fp_enc(uint64_t *a, uint64_t *c);
 
+extern void half_mul(uint64_t *a, uint64_t *b, uint64_t *c);
+
 const struct uint p = {{
     0x1b81b90533c6c87b, 0xc2721bf457aca835, 0x516730cc1f0b4f25, 0xa7aac6c567f35507,
     0x5afbfcc69322c9cd, 0xb42d083aedc88c42, 0xfc8ab0d15e3e4c4a, 0x65b48e8f740f89bf,
 }};
+const struct uint r_squared_mod_p_here = {{
+    0x36905b572ffc1724, 0x67086f4525f1f27d, 0x4faf3fbfd22370ca, 0x192ea214bcc584b1,
+    0x5dae03ee2f5de3d0, 0x1e9248731776b371, 0xad5f166e20e4f52d, 0x4ed759aea6f3917e,
+}};
+
 
 const uint64_t big_one[16] = {0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
 
@@ -61,11 +68,10 @@ void mul_some() {
 }
 
 void test_encode(){
-    uint a = {{0x1, 0x0, 0x0, 0x0,0x0,0x0,0x0, 0x1}};
+    uint64_t a[8] = {0x1, 0x0,0x0,0x0,0x0,0x0,0x0,0x0};
+    uint64_t result[8] = {0x0};
+    monte_mul(a, r_squared_mod_p_here.c,result);
     
-    uint encoded = {{0}};
-    fp_enc(a.c, encoded.c);
-    uint_print(&encoded);
 }
 
 int main() {
