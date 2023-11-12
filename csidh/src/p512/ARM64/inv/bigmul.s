5b9e7f7cbe66089d87b4a1805bba88c5c495133f
Lukas <bamert@protonmail.com>
2023-11-12 14:02:25 +0100

    almost finished 512 bit subtractive karatsuba

--- csidh/src/p512/ARM64/inv/bigmul.s
+++ csidh/src/p512/ARM64/inv/bigmul.s
@@ -29,26 +29,27 @@
 
 // 𝐴𝐿 ∙ 𝐵𝐿 + 𝐴𝐻 ∙ 𝐵𝐻 − |𝐴𝐻 − 𝐴𝐿| ∙ |𝐵𝐻 − 𝐵𝐿| = RH · 2^256 + (RL + RH − RM ) · 2^128 + RL
 // !!messes with A0-B3!!
-// needs 21 registers
-.macro MUL256_KARATSUBA A0, A1, A2, A3, B0, B1, B2, B3, C0, C1, C2, C3, C4, C5, C6, C7, T0, T1, T2, T3, T4, T5
+// needs 20 registers
+.macro MUL256_KARATSUBA A0, A1, A2, A3, B0, B1, B2, B3, C0, C1, C2, C3, C4, C5, C6, C7, T0, T1, T2, T3, T4
     // AL * BL into C0-C3 = L
     MUL128x128 \A0, \A1, \B0, \B1, \C0, \C1, \C2, \C3, \T0
     // AH * BH into C4-C7 = H
     MUL128x128 \A2, \A3, \B2, \B3, \C4, \C5, \C6, \C7, \T0
 
-        // + L*2^128 from (L+H-M)*2^128(2 words) 
+    // + L*2^128 from (L+H-M)*2^128(2 words) 
     adds \T0, \C2, \C0
     adcs \T1, \C3, \C1
     adcs \T2, \C4, \C2
     adcs \T3, \C5, \C3
-    adc  \T4, \C6, xzr
+    adc  \T4, xzr, xzr
 
     // + H*2^128 from (L+H-M)*2^128(2 words)
     adds \C2, \T0, \C4
     adcs \C3, \T1, \C5
     adcs \C4, \T2, \C6
     adcs \C5, \T3, \C7
-    adc \C6, \T4, xzr
+    adcs \C6, \T4, \C6
+    adcs \C7, xzr, \C7
 
     // |AH - AL| into A0-A1 sign in A2
     subs \A0, \A2, \A0
@@ -102,6 +103,134 @@
 
 .endm
 
+// not using x18 and x29 as apple states -> x3-x17, x19-x28 = 15 + 10 = 25 registers
+.global _uint_mul_512
+_uint_mul_512:
+    sub sp, sp, #240
+    // save registers
+    stp lr, x0, [sp, #0] 
+    stp x1, x2, [sp, #16]
+    stp x19, x20, [sp, #32]
+    stp x21, x22, [sp, #48]
+    stp x23, x24, [sp, #64]
+    stp x25, x26, [sp, #80]
+    stp x27, x28, [sp, #96]
+
+    // Load  low A
+    ldp x3, x4, [x0, #0]
+    ldp x5, x6, [x0, #16]
+    // Load low B 
+    ldp x7, x8, [x1, #0]
+    ldp x9, x10, [x1, #16]
+
+    // AL x BL = L = x20-x27
+    // AL x BL = L = x11-x19
+    MUL256_KARATSUBA x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x19, x20, x21, x22, x23
+    stp x11, x12, [x2, #0]
+    stp x13, x14, [x2, #16]
+
+    // load high A
+    ldp x3, x4, [x0, #32]
+    ldp x5, x6, [x0, #48]
+    // load high B
+    ldp x7, x8, [x1, #32]
+    ldp x9, x10, [x1, #48]
+
+    // AH x BH = H = x20-x27
+    // carefull we need x11, x12 as temp
+    MUL256_KARATSUBA x3, x4, x5, x6, x7, x8, x9, x10, x20, x21, x22, x23, x24, x25, x26, x27, x28, x30, x11, x12
+    ldp x11, x12, [x2, #0] // restore x11,x12
+
+    // +L*2^256 from (L+H-M)*2^256(4 words offset) 
+
+    adds x3, x15, x11 // T0 = C4 + C0
+    adcs x4, x16, x12 // T1 = C5 + C1
+    adcs x5, x17, x13 // T2 = C6 + C2
+    adcs x6, x19, x14 // T3 = C7 + C3
+    adcs x7, x20, x15 // T4 = C8 + C4
+    adcs x8, x21, x16 // T5 = C9 + C5
+    adcs x9, x22, x17 // T6 = C10 + C6
+    adcs x10, x23, x19 // T7 = C11 + C7
+    adcs x30, xzr, xzr // x30 = carry
+
+    // +H*2^256 from (L+H-M)*2^256(4 words offset)
+    adds x15, x3, x20 // C4 = T0 + C8
+    adcs x16, x4, x21 // C5 = T1 + C9
+    adcs x17, x5, x22 // C6 = T2 + C10
+    adcs x19, x6, x23 // C7 = T3 + C11
+
+    adcs x20, x7, x24 // C8 = T4 + C12
+    adcs x21, x8, x25 // C9 = T5 + C13
+    adcs x22, x9, x26 // C10 = T6 + C14
+    adcs x23, x10, x27 // C11 = T7 + C15
+    //carry prop
+    adcs x24, x30, x24 // C12 = carry
+    adcs x25, x25, xzr
+    adcs x26, x26, xzr
+    adcs x27, x27, xzr
+
+    stp x24, x25, [x2, #96] // store C12-C15
+    stp x26, x27, [x2, #112]
+
+
+    // |AH - AL| x3-x6
+    ldp x3, x4, [x0, #32]
+    ldp x5, x6, [x0, #48]
+    ldp x7, x8, [x0, #32]
+    ldp x9, x10, [x0, #48]
+
+    subs x3, x3, x7
+    sbcs x4, x4, x8
+    sbcs x5, x5, x9
+    sbcs x6, x6, x10
+    // calculate two's complement if negative 
+    cinv x3, x3, LO
+    cinv x4, x4, LO
+    cinv x5, x5, LO
+    cinv x6, x6, LO
+    cset x30, LO
+    adds x3, x3, x30
+    adcs x4, x4, xzr
+    adcs x5, x5, xzr
+    adcs x6, x6, xzr
+
+
+
+    // |BH - BL| x7-x10
+    ldp x7, x8, [x1, #0]
+    ldp x9, x10, [x1, #16]
+    ldp x11, x12, [x1, #32] // we already saved x11-x14 (lowest pair)
+    ldp x13, x14, [x1, #48]
+
+    subs x7, x7, x11
+    sbcs x8, x8, x12
+    sbcs x9, x9, x13
+    sbcs x10, x10, x14
+
+    // calculate two's complement if negative
+    cinv x7, x7, LO
+    cinv x8, x8, LO
+    cinv x9, x9, LO
+    cinv x10, x10, LO
+    cset x11, LO
+    adds x7, x7, x11
+    adcs x8, x8, xzr
+    adcs x9, x9, xzr
+    adcs x10, x10, xzr
+
+
+    eor x30, x30, x11 // combined sign
+    sub x30, x30, #1 // if 0 -> fffffff, if 1 -> 0
+    //M = |AH - AL| * |BH - BL| = x11-x14, x25-x28
+    MUL256_KARATSUBA x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x25, x26, x27, x28, T0, T1, T2, T3, T4 
+
+    // 2's complement if sign was 0  --> into x3-x10
+    
+
+
+    ret
+
+
 
 //  Operation: c [x2] = a [x0] * b [x1]
 //a = 8 words, b = 8 words, c = 8 words
@@ -126,7 +255,7 @@ _uint_mul_lower:
     ldp x9, x10, [x1, #16]
 
     // AL x BL = L = x20-x27
-    MUL256_KARATSUBA x3, x4, x5, x6, x7, x8, x9, x10, x20, x21, x22, x23, x24, x25, x26, x27, x11, x12, x13, x14, x15, x16
+    MUL256_KARATSUBA x3, x4, x5, x6, x7, x8, x9, x10, x20, x21, x22, x23, x24, x25, x26, x27, x11, x12, x13, x14, x15
     stp x20, x21, [x2, #0]
     stp x22, x23, [x2, #16]
     stp x24, x25, [x2, #32]
